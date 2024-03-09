05e44a8304728bf3fc401af07225e5ec4996679b
Canul <canuladriana24@gmail.com>
2024-03-09 16:06:43 -0600

    primer commit calculadora

---
+++ app/src/main/java/com/example/calculadoraapp/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.calculadoraapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.calculadoraapp.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
