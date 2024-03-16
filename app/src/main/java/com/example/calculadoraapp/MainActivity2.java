1dc0bf47ba22cf4704078c0aac968dac3e9132b2
Canul <canuladriana24@gmail.com>
2024-03-15 23:16:15 -0600

    comienzos de la logica de la calculadora

--- app/src/main/java/com/example/calculadoraapp/MainActivity2.java
+++ app/src/main/java/com/example/calculadoraapp/MainActivity2.java
@@ -2,11 +2,13 @@ package com.example.calculadoraapp;
 
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
@@ -16,12 +18,55 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.calculadoraapp.databinding.ActivityMain2Binding;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+
+    TextView resultTV,solutionTv;
+    MaterialButton buttonC,buttonBrack0pen,buttonBrackClose;
+    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAC,buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        resultTV = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
+
+        assignId(buttonC,R.id.button_c);
+        assignId(buttonBrack0pen,R.id.button_open_bracket);
+        assignId(buttonBrackClose,R.id.button_close_bracket);
+        assignId(buttonDivide,R.id.button_divide);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonEquals,R.id.button_equals);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_ac);
+        assignId(buttonDot,R.id.button_dot);
+
+
+
     }
 
+    void assignId(MaterialButton btn,int  id ){
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+    MaterialButton button = (MaterialButton) view;
+    String buttonText = button.getText().toString();
+    solutionTv.setText(buttonText);
+    }
 }
