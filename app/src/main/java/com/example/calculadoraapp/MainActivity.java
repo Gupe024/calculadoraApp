40bb63afed82ac563e3d600e5fa8c3e271630521
Canul <canuladriana24@gmail.com>
2024-03-19 02:33:05 -0600

    cambios en la interfasz y su logica

--- app/src/main/java/com/example/calculadoraapp/MainActivity.java
+++ app/src/main/java/com/example/calculadoraapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.calculadoraapp;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText usuario, password;
+    Button iniciar;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        iniciar = findViewById(R.id.iniciar);
+        usuario = findViewById(R.id.usuario);
+        password = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences("Mis preferencias", Context.MODE_PRIVATE);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuario.getText().toString();
+                String pass = password.getText().toString();
+
+                usuario.setText("");
+                password.setText("");
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("usuario", user);
+                editor.putString("password", pass);
+                editor.apply();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
