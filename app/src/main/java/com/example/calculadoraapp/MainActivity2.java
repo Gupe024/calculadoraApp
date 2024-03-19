8d8e50d4ca7bad2ca0957124fa62488ac65d0289
Canul <canuladriana24@gmail.com>
2024-03-19 12:29:20 -0600

    Logica de la caluladora

--- app/src/main/java/com/example/calculadoraapp/MainActivity2.java
+++ app/src/main/java/com/example/calculadoraapp/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.calculadoraapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+    TextView resultTv, solutionTv;
+    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        resultTv=findViewById(R.id.result_Tv);
+        solutionTv=findViewById(R.id.solution_Tv);
+
+
+
+
+    }
+
+    void assingId(MaterialButton btn,int id){
+        btn=findViewById(id);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
